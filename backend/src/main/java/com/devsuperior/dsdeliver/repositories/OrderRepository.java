7263793a77731c55112981bfb16bd5ec92a87920
nataliegusso <nataliegusso@gmail.com>
2021-01-06 18:27:54 -0300

    GET /orders

--- backend/src/main/java/com/devsuperior/dsdeliver/repositories/OrderRepository.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/repositories/OrderRepository.java
@@ -1,9 +1,17 @@
 package com.devsuperior.dsdeliver.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.dsdeliver.entities.Order;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
+	//Qdo eu crio a consulta e não uso a pronta
+	@Query("SELECT DISTINCT obj FROM Order obj JOIN FETCH obj.products "   //SELECT DISTINCT apelido FROM NomeDaClasse apelido JOIN FETCH apelido.nomeDaColeçãoSet
+			+ " WHERE obj.status = 0 ORDER BY obj.moment ASC")   //Where apelido.nomeDaEnumeração = númEnumeração ORDER BY apelido.tempo ordem
+	List<Order> findOrdersWithProducts();
+	
 }
